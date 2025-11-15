13fe3e30c2cd291c558b0778cefcd12ddba7ae73
Sofia <dannavelandia6969@gmail.com>
2025-11-15 02:49:37 -0500

    otro ajuste mas

--- mi-proyecto/src/components/FooterComponent.vue
+++ mi-proyecto/src/components/FooterComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer mt-auto py-3 bg-dark text-white">
     <div class="container text-center">
-      <span>&copy; 2024 Intimacy Shop. Todos los derechos reservados.</span>
+      <span>&copy; 2025 Intimacy Shop. Todos los derechos reservados.</span>
     </div>
   </footer>
 </template>
